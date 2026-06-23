f7aef62336ff4a0a1ba28ff339397b9ae62ed9d5
hana-lee-dev <llsingingcatll@gmail.com>
2026-06-23 20:29:08 +0800

    push

--- FarmOrderSystem/Program.cs
+++ FarmOrderSystem/Program.cs
@@ -16,7 +16,7 @@ namespace FarmOrderSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmOrderQuery());
+            Application.Run(new frmOrder());
         }
     }
 }
